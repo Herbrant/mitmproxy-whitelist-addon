4d8903d55d2f878863fdc3774516aa7360aa003c
herbrant <cdavide98carnemolla@gmail.com>
2021-06-12 23:50:27 +0200

    feat: loads domains and urls from configuration files

--- whitelist.py
+++ whitelist.py
@@ -1,20 +1,37 @@
+from sys import path
 import typing
-
-from mitmproxy import ctx, exceptions, flowfilter, http, version
+import argparse
+from mitmproxy import ctx, http
 
 class Whitelist:
-    def __init__(self):
-        self.whitelist = []
+    DOMAINS_CONFIGURATION_FILE="/usr/local/mitmproxy/whitelist/domains"
+    URLS_CONFIGURATION_FILE="/usr/local/mitmproxy/whitelist/urls"
 
+    def __init__(self):
+        self.allowedDomains = []
+        self.allowedUrls = []
+    
+    def loadDomains(self):
+        with open(self.DOMAINS_CONFIGURATION_FILE) as domains:
+            self.allowedDomains = list(filter(None, domains.read().split('\n')))
+    
+    def loadUrls(self):
+        with open(self.URLS_CONFIGURATION_FILE) as urls:
+            self.allowedUrls = list(filter(None, urls.read().split('\n')))
+
+    def loadConfigFiles(self):
+        self.loadDomains()
+        self.loadUrls()
+    
     def load(self, loader):
-        self.whitelist.append("google.com")
-        self.whitelist.append("neverssl.com")
+        self.loadConfigFiles()
+        ctx.log.info("Allowed Domain List: {}".format(self.allowedDomains))
+        ctx.log.info("Allowed Urls List: {}".format(self.allowedUrls))
 
     def request(self, flow: http.HTTPFlow) -> None:
-        #ctx.log.info("flow.request.url: {}".format(flow.request.url))
         killSession = True
 
-        for domain in self.whitelist:
+        for domain in self.allowedDomains:
             if flow.request.pretty_host.endswith(domain):
                 killSession = False
 
